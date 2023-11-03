9710ef8894306d4a1aef0dd7b000be2453ad2fb7
sharonal10 <sharonal@stanford.edu>
2023-11-03 00:17:21 -0700

    use ckpt attempt1

--- config/base.py
+++ config/base.py
@@ -25,7 +25,8 @@ def get_config():
     # resume training from a checkpoint. either an exact checkpoint directory (e.g. checkpoint_50), or a directory
     # containing checkpoints, in which case the latest one will be used. `config.use_lora` must be set to the same value
     # as the run that generated the saved checkpoint.
-    config.resume_from = ""
+    # config.resume_from = ""
+    config.resume_from = "/viscam/projects/langint/sharonal/ddpo-pytorch/ddpo_pytorch/assets"
     # whether or not to use LoRA. LoRA reduces memory usage significantly by injecting small weight matrices into the
     # attention layers of the UNet. with LoRA, fp16, and a batch size of 1, finetuning Stable Diffusion should take
     # about 10GB of GPU memory. beware that if LoRA is disabled, training will take a lot of memory and saved checkpoint
